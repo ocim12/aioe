5a69d30d77864eb0a4e2a160534f33e60010a569
ocim12 <bartosz.ocimek@gmail.com>
2020-11-16 17:11:58 +0100

    ocim

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, {useState} from 'react';
+function App(){
+  const [number,setNumber] = useState(5);
+  return(
+    <div className="App">
+      <div>{number}</div>
+      <button onClick={() => setNumber(number+1)}>+</button>
+      <button onClick={()=> setNumber(number-1)}>-</button>
+    </div>
+  );
 
-import './custom.css'
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
 }
+
+export default App;
