3dca3856fa483ab8a6d0df7f1f5b8c25073a4450
Freeskier <snowwolf866@gmail.com>
2020-11-17 16:30:08 +0100

    Emil - pierwsza stabilna

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace aioe.Models
+{
+    public class User
+    {
+        public int UserID {get; set;}
+        public string Name {get; set;}
+        public string Password {get; set;}
+    }
+}
